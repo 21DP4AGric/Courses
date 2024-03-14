9ad668243039520c23c671c8534ea09ffb4bce82
Me <Me>
2024-03-14 14:19:07 +0200

    everything

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/courses', [CourseController::class, 'store']);
+Route::get('/courses', [CourseController::class,'index']);
